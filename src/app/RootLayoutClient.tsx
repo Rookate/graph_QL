f15ef5131220521c7030b5177e8ad7823421bcd3
Gabriel K <gab270b@gmail.com>
2025-03-03 10:01:53 +0100

    clean variable

--- src/app/RootLayoutClient.tsx
+++ src/app/RootLayoutClient.tsx
@@ -5,10 +5,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import LogoutSvg from "@/components/ui/logoutSvg";
 import { logoutUser } from "./lib/auth";
-import { useUser } from "./context/userContext";
 
 export default function RootLayoutClient({ children }: { children: React.ReactNode }) {
-    const { user } = useUser();
     const pathname = usePathname();
 
     const validRoutes = ["/", "/dashboard", "/profile", "/settings", "/auth/login", '/audit'];
